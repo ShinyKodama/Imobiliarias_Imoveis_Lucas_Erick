b815db534eee86681ed2bef357224ceb0dfd0665
ShinyKodama <lbazzan.lucas@gmail.com>
2026-09-22 11:36:33 -0300

    Primeiro Commit

---
+++ MVC/views/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+    <title> Wellerson | Imobiliárias </title>
+</head>
+<style>
+    .nav-link a{ font-size: 1.2rem !important; text-decoration: none !important; color: #222; }
+
+</style>
+<body>
+    <div class="container-fluid">
+        <div class="navbar align-items-center justify-content-end px-4 gap-4 bg-white">
+            <label for="user-configs"> Espaço do Usuário </label>
+            <select name="userConfigs" id="user-configs">
+                 
+            </select>
+            <div class="nav-link"> 
+                <a href="#"> <object data="../../images/icons/icon-instagram.svg" width="50" height="50"></object> </a> 
+            </div>
+            <div class="nav-link"> 
+                <a href="#Inicio"> <object data="../../images/icons/icon-twitter.svg" width="50" height="50"></object> </a>  
+            </div>
+            <div class="nav-link"> 
+                <a href="#Inicio"> <object data="../../images/icons/icon-whatsapp.svg" width="50" height="50"></object>  </a>
+            </div>
+        </div>    
+    </div>
+    <script src="../../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
